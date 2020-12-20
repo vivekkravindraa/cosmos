020bbc04effe9198bf421a8c3a18f688d12c8f69
Vivek Ravindra <vivekkravindraa@Viveks-MacBook-Air.local>
2020-12-20 05:47:02 +0530

    Updates

---
+++ precache-manifest.def671c38e47dccedd1a191b732b1ae4.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e819da6c1c13fbf6fe4759976a3faa79",
+    "url": "/cosmos/index.html"
+  },
+  {
+    "revision": "53cd674f1e3728d088f0",
+    "url": "/cosmos/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "d7551606a260092ff4d4",
+    "url": "/cosmos/static/css/main.7a236b9a.chunk.css"
+  },
+  {
+    "revision": "53cd674f1e3728d088f0",
+    "url": "/cosmos/static/js/2.5869b3d0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cosmos/static/js/2.5869b3d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7551606a260092ff4d4",
+    "url": "/cosmos/static/js/main.38552561.chunk.js"
+  },
+  {
+    "revision": "e18ef7ae4555c278396f",
+    "url": "/cosmos/static/js/runtime-main.aafc5381.js"
+  },
+  {
+    "revision": "7920b16d8e7307fb1cd28b6db8d188ac",
+    "url": "/cosmos/static/media/cosmosAboutBanner.7920b16d.jpg"
+  },
+  {
+    "revision": "64bc5b9a492c9ff954f8276ec078232c",
+    "url": "/cosmos/static/media/cosmosAddidas.64bc5b9a.png"
+  },
+  {
+    "revision": "c3d9171c7243783e3ea1f70dd6c3333d",
+    "url": "/cosmos/static/media/cosmosAllSportsS.c3d9171c.png"
+  },
+  {
+    "revision": "51487ffc933d18283019815211f6f2a8",
+    "url": "/cosmos/static/media/cosmosAsics.51487ffc.png"
+  },
+  {
+    "revision": "6f9bb62bbc13221da135c48a9b7d2557",
+    "url": "/cosmos/static/media/cosmosBackpackS.6f9bb62b.png"
+  },
+  {
+    "revision": "0bd450c5ba17684b30a5467058a9fb8f",
+    "url": "/cosmos/static/media/cosmosBadmintonS.0bd450c5.png"
+  },
+  {
+    "revision": "2ede96e34fdc0925f8dd545ccbe603d9",
+    "url": "/cosmos/static/media/cosmosBannerOneS.2ede96e3.png"
+  },
+  {
+    "revision": "94139a12c780be0fd138cf35067cfe7a",
+    "url": "/cosmos/static/media/cosmosBikeS.94139a12.png"
+  },
+  {
+    "revision": "25323f44086c746f0fe3bfd6c420b55a",
+    "url": "/cosmos/static/media/cosmosCampingS.25323f44.png"
+  },
+  {
+    "revision": "75f9a5071e087b548954c7596b1737ce",
+    "url": "/cosmos/static/media/cosmosCricketS.75f9a507.png"
+  },
+  {
+    "revision": "9c614dfadd2b55c5f61838a0180e3244",
+    "url": "/cosmos/static/media/cosmosDealFourS.9c614dfa.png"
+  },
+  {
+    "revision": "76a70a24e58dfb97b9d0ac2f364c26a6",
+    "url": "/cosmos/static/media/cosmosDealOneS.76a70a24.png"
+  },
+  {
+    "revision": "94ab4159a853b56973692d84e650433b",
+    "url": "/cosmos/static/media/cosmosDealThreeS.94ab4159.png"
+  },
+  {
+    "revision": "7ceb797b173bd8ab0eefd21a77b1ee9e",
+    "url": "/cosmos/static/media/cosmosDealTwoS.7ceb797b.png"
+  },
+  {
+    "revision": "8463cf25a14e2040040e616ba7e07e3d",
+    "url": "/cosmos/static/media/cosmosEquipments.8463cf25.jpg"
+  },
+  {
+    "revision": "7930573ee67f63485f69e3a738dba92f",
+    "url": "/cosmos/static/media/cosmosFeaturedFiveS.7930573e.png"
+  },
+  {
+    "revision": "378b9d1130f03338194b4394781a77a7",
+    "url": "/cosmos/static/media/cosmosFeaturedFourS.378b9d11.png"
+  },
+  {
+    "revision": "7366192b6faba1b41671ca1d6fad5533",
+    "url": "/cosmos/static/media/cosmosFeaturedOneS.7366192b.png"
+  },
+  {
+    "revision": "8a62989089d33b6a64f20f2cf3c4a742",
+    "url": "/cosmos/static/media/cosmosFeaturedSixS.8a629890.png"
+  },
+  {
+    "revision": "392caae92adaa8d65b9f8a63ce1a43c8",
+    "url": "/cosmos/static/media/cosmosFeaturedThreeS.392caae9.png"
+  },
+  {
+    "revision": "753eac4576b497f2d7a9a47bfd10548b",
+    "url": "/cosmos/static/media/cosmosFeaturedTwoS.753eac45.png"
+  },
+  {
+    "revision": "2a0db7cdae95a89cbcbb9d14afe9755f",
+    "url": "/cosmos/static/media/cosmosFootballS.2a0db7cd.png"
+  },
+  {
+    "revision": "31e759e838e988c8978394a91fca511a",
+    "url": "/cosmos/static/media/cosmosGolfS.31e759e8.png"
+  },
+  {
+    "revision": "b09e59b2d585f52d5e81a5f794dd4c9c",
+    "url": "/cosmos/static/media/cosmosHockeyS.b09e59b2.png"
+  },
+  {
+    "revision": "339730ca720c5fbaab526da586313122",
+    "url": "/cosmos/static/media/cosmosIceS.339730ca.png"
+  },
+  {
+    "revision": "69490b10d57d2a174bbd164c73886152",
+    "url": "/cosmos/static/media/cosmosInfrastructure.69490b10.jpg"
+  },
+  {
+    "revision": "5ba7b7e4ef22d60dca5010928bfc858e",
+    "url": "/cosmos/static/media/cosmosKidsS.5ba7b7e4.png"
+  },
+  {
+    "revision": "aece77fed06e076fb320df8f698abf7c",
+    "url": "/cosmos/static/media/cosmosLining.aece77fe.png"
+  },
+  {
+    "revision": "51a9aabf6d6a68b0052167d461d565f8",
+    "url": "/cosmos/static/media/cosmosLogoBlackS.51a9aabf.png"
+  },
+  {
+    "revision": "092dd95f26290353cbec6ec7c4310469",
+    "url": "/cosmos/static/media/cosmosMenS.092dd95f.png"
+  },
+  {
+    "revision": "527bb7feda93ae338fcc3b8426ce0348",
+    "url": "/cosmos/static/media/cosmosNike.527bb7fe.png"
+  },
+  {
+    "revision": "b86bdc93d941b12984ed69932851f64e",
+    "url": "/cosmos/static/media/cosmosOffers.b86bdc93.png"
+  },
+  {
+    "revision": "ef789742dfde1298c56be1b0e8c9b372",
+    "url": "/cosmos/static/media/cosmosProfile.ef789742.png"
+  },
+  {
+    "revision": "948a69839c6953f351f9c1b5387a26fc",
+    "url": "/cosmos/static/media/cosmosPuma.948a6983.png"
+  },
+  {
+    "revision": "394e991fbd63b14d3a1b93eea5d855e9",
+    "url": "/cosmos/static/media/cosmosRacketS.394e991f.png"
+  },
+  {
+    "revision": "0016479bd6dacbce8e611b20afbd9a4d",
+    "url": "/cosmos/static/media/cosmosRunnersEightS.0016479b.png"
+  },
+  {
+    "revision": "1fd46e3023b1913dc8ee74ec29bda9de",
+    "url": "/cosmos/static/media/cosmosRunnersFiveS.1fd46e30.png"
+  },
+  {
+    "revision": "65d0980d0b0141b1884d6233999b19df",
+    "url": "/cosmos/static/media/cosmosRunnersFourS.65d0980d.png"
+  },
+  {
+    "revision": "c30110ecff18be0da7818b6890750185",
+    "url": "/cosmos/static/media/cosmosRunnersNineS.c30110ec.png"
+  },
+  {
+    "revision": "ffc40e2383d601b97a26f708d3322089",
+    "url": "/cosmos/static/media/cosmosRunnersOneS.ffc40e23.png"
+  },
+  {
+    "revision": "4e8e1ac6237d67cd422a2bd11e1ab51d",
+    "url": "/cosmos/static/media/cosmosRunnersSevenS.4e8e1ac6.png"
+  },
+  {
+    "revision": "9e5060f367c072c75caba7e969e4c60e",
+    "url": "/cosmos/static/media/cosmosRunnersSixS.9e5060f3.png"
+  },
+  {
+    "revision": "753eac4576b497f2d7a9a47bfd10548b",
+    "url": "/cosmos/static/media/cosmosRunnersThreeS.753eac45.png"
+  },
+  {
+    "revision": "97ab2352a8d6d2e7cb5cb20699a731d6",
+    "url": "/cosmos/static/media/cosmosRunnersTwoS.97ab2352.png"
+  },
+  {
+    "revision": "4a7289a81286cf7ed6dac577b38a5abd",
+    "url": "/cosmos/static/media/cosmosSkatingS.4a7289a8.png"
+  },
+  {
+    "revision": "6f409b4a590c29b85e3a5c3e788e4f79",
+    "url": "/cosmos/static/media/cosmosSponsoredAdS.6f409b4a.png"
+  },
+  {
+    "revision": "b1a1c9fe91333f60e84bde49abdf0c1d",
+    "url": "/cosmos/static/media/cosmosSportsS.b1a1c9fe.png"
+  },
+  {
+    "revision": "5b5c25d228e8b8d6e0d84ff7b31d783f",
+    "url": "/cosmos/static/media/cosmosStoreBannerS.5b5c25d2.png"
+  },
+  {
+    "revision": "923ad1667f97424986c9b23c8924714c",
+    "url": "/cosmos/static/media/cosmosSwimmingS.923ad166.png"
+  },
+  {
+    "revision": "57b9ef3b9cc4a919637ddd389872d3ca",
+    "url": "/cosmos/static/media/cosmosWatchesS.57b9ef3b.png"
+  },
+  {
+    "revision": "f7f977363e447cf7e8176540a0fd6c24",
+    "url": "/cosmos/static/media/cosmosWomenS.f7f97736.png"
+  }
+]);
